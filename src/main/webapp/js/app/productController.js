c887d2b3f13a185d87b5da8e43aa287a4c4a3eee
MunishBharmoria <MunishBharmoria@DESKTOP-MAAGEC7>
2018-12-27 23:38:29 +0530

    updated

--- src/main/webapp/js/app/productController.js
+++ src/main/webapp/js/app/productController.js
@@ -1,12 +1,17 @@
 'use strict';
 
 var module = angular.module('shopModule', ['ngRoute']);
-angular.module('shopModule').controller('shopController', [ '$scope', '$http', function($scope, $http,$interval) {
+angular.module('shopModule').controller('shopController', [ '$scope', '$http', '$window', function($scope, $http, $interval) {
 	
-	$scope.initialize = function(){
+		$scope.initialize = function(){
 		$scope.listingImageWidth = "300";
 		$scope.listingImageHeight = "150";
 		$scope.enableSection = "Home";
+		$scope.prodUrlPrifix = "";  // for production value should be "/nx-1" & for local it should be ""
+		$scope.openCloseTime =["12:00 AM","12:30 AM","01:00 AM","01:30 AM","02:00 AM","02:30 AM","03:00 AM","03:30 AM","04:00 AM","04:30 AM","05:00 AM","05:30 AM",
+								"06:00 AM","06:30 AM","07:00 AM","07:30 AM","08:00 AM","08:30 AM","09:00 AM","09:30 AM","10:00 AM","10:30 AM","11:00 AM","11:30 AM",
+								"12:00 PM","12:30 PM","01:00 PM","01:30 PM","02:00 PM","02:30 PM","03:00 PM","03:30 PM","04:00 PM","04:30 PM","05:00 PM","05:30 PM",
+								"06:00 PM","06:30 PM","07:00 PM","07:30 PM","08:00 PM","08:30 PM","09:00 PM","09:30 PM","10:00 PM","10:30 PM","11:00 PM","11:30 PM"];
 		advertisementHome();
 	}
 	
@@ -16,7 +21,7 @@ angular.module('shopModule').controller('shopController', [ '$scope', '$http', f
 	$scope.getNxDirCategories = function() { 
 		$http({
 			method : "GET",
-			url:"/getNxDirCategories",
+			url:$scope.prodUrlPrifix + "/getNxDirCategories",	
 			//url:"order/summary/"+'2017-11-01'+"/"+'2017-11-07',
 			//data : angular.toJson(),
 			headers :{
@@ -33,7 +38,7 @@ angular.module('shopModule').controller('shopController', [ '$scope', '$http', f
 	$scope.getNxDirThumnailCategories = function() { 
 		$http({
 			method : "GET",
-			url:"/getNxCategoriesThumbnails",
+			url:$scope.prodUrlPrifix + "/getNxCategoriesThumbnails",
 			//url:"order/summary/"+'2017-11-01'+"/"+'2017-11-07',
 			//data : angular.toJson(),
 			headers :{
@@ -50,7 +55,7 @@ angular.module('shopModule').controller('shopController', [ '$scope', '$http', f
 	$scope.getNxRestaurantList = function() { 
 		$http({
 			method : "GET",
-			url:"/getNxRestaurentList",
+			url:$scope.prodUrlPrifix + "/getNxRestaurentList",
 			//url:"order/summary/"+'2017-11-01'+"/"+'2017-11-07',
 			//data : angular.toJson(),
 			headers :{
@@ -68,7 +73,7 @@ angular.module('shopModule').controller('shopController', [ '$scope', '$http', f
 	$scope.getNxHospitalList = function() { 
 		$http({
 			method : "GET",
-			url:"/getNxHospitalList",
+			url:$scope.prodUrlPrifix + "/getNxHospitalList",
 			//url:"order/summary/"+'2017-11-01'+"/"+'2017-11-07',
 			//data : angular.toJson(),
 			headers :{
@@ -85,7 +90,7 @@ angular.module('shopModule').controller('shopController', [ '$scope', '$http', f
 	$scope.getNxSchoolList = function() { 
 		$http({
 			method : "GET",
-			url:"/getNxSchoolList",
+			url:$scope.prodUrlPrifix + "/getNxSchoolList",
 			//url:"order/summary/"+'2017-11-01'+"/"+'2017-11-07',
 			//data : angular.toJson(),
 			headers :{
@@ -102,7 +107,7 @@ angular.module('shopModule').controller('shopController', [ '$scope', '$http', f
 	$scope.getNxDoctorList = function() { 
 		$http({
 			method : "GET",
-			url:"/getNxDoctorList",
+			url:$scope.prodUrlPrifix + "/getNxDoctorList",
 			//url:"order/summary/"+'2017-11-01'+"/"+'2017-11-07',
 			//data : angular.toJson(),
 			headers :{
@@ -119,7 +124,7 @@ angular.module('shopModule').controller('shopController', [ '$scope', '$http', f
 	$scope.getNxPlaySchoolList = function() { 
 		$http({
 			method : "GET",
-			url:"/getNxPlaySchoolList",
+			url:$scope.prodUrlPrifix + "/getNxPlaySchoolList",
 			//url:"order/summary/"+'2017-11-01'+"/"+'2017-11-07',
 			//data : angular.toJson(),
 			headers :{
@@ -133,11 +138,66 @@ angular.module('shopModule').controller('shopController', [ '$scope', '$http', f
 	
 	$scope.getMyAction= function(url) { 
 		$scope.enableSection = url;
-		$scope.enableListing = true;
+		//$scope.enableListing = true;
 	}
 	
-	$scope.setListingFalse= function() { 
+	/* $scope.setListingFalse= function() { 
 		$scope.enableListing = false;
+	} */
+
+	/*$scope.registerBusiness = function() { 
+		$http({
+			method : "GET",
+			url:$scope.prodUrlPrifix + "/getNxPlaySchoolList",
+			//url:"order/summary/"+'2017-11-01'+"/"+'2017-11-07',
+			//data : angular.toJson(),
+			headers :{
+				'Content-Type' : 'application/json'}
+		}).success(function(response) {
+			  console.log('response: ' + response);
+			  $scope.nxPlaySchoolsList = response;
+		});
+	}*/
+
+	$scope.registerBusiness = function() { 
+		$scope.enableSection = "register";
+	}
+
+	$scope.aboutUs = function() { 
+		$scope.enableSection = "aboutUs";
+	}
+
+	$scope.contactUs = function() { 
+		$scope.enableSection = "aboutUs";
+	}
+
+	$scope.submitListing = function (businessName, category, businessAdd, businessContactNo, openHours, closeHours, ownOpenCloseHours, imageSrc) {
+		if(businessName == undefined || category == undefined || businessAdd == undefined || businessContactNo == undefined ||
+			businessName.trim().length == 0 || category.trim().length == 0 || businessAdd.trim().length == 0 || businessContactNo.trim().length == 0)
+		{
+			alert("Enter Manadatory Fields");
+			alert('[{businessName : "' + businessName + '"},{businessCatagory : "' + businessCatagory + '"},{businessAdd :"' + businessAdd + '"},{businessContactNo :"' + businessContactNo +
+			'"},{ openHours :"' + openHours + '"},{closeHours :"' + closeHours + '"},{ownOpenCloseHours :"' + ownOpenCloseHours + '"},{ imageSrc :"' + imageSrc + '"}]');
+			return;
+		}
+		$scope.listingValues = '[{businessName : "' + businessName + '"},{businessCatagory : "' + category + '"},{businessAdd :"' + businessAdd + '"},{businessContactNo :"' + businessContactNo +
+			'"},{ openHours :"' + openHours + '"},{closeHours :"' + closeHours + '"},{ownOpenCloseHours :"' + ownOpenCloseHours + '"},{ imageSrc :"' + imageSrc + '"}]';
+		$http({
+			method: "POST",
+			url: $scope.prodUrlPrifix + "/submitListing" + "/" + $scope.listingValues,
+			//url:"order/summary/"+'2017-11-01'+"/"+'2017-11-07',
+			//data : angular.toJson(),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		}).success(function (response) {
+			console.log('response: ' + response);
+			$scope.submitStatus = response;
+		});
+		$scope.businessName = null;
+		$scope.businessAdd = "";
+		$scope.businessContactNo = "";
+		alert("You Request Submitted For Approval.");
 	}
 
 } ]);
@@ -160,11 +220,33 @@ function popup(mylink, windowname) {
 	return false;
 }
 
+
+
+
 function advertisementHome()
 {
 	
-	window.open("http://localhost:8080/img/doctor/doctor_DrKamanaPurohit.jpeg", "DoctorKamana", 
-			'width=200,height=200,scrollbars=yes,screenX=850,screenY=850,top=50');
-	window.open("http://localhost:8080/img/restaurant/restaurant_MirchMasalaChineseFastFood.jpeg", "RestaurantMirchMasala", 
-			'width=200,height=200,scrollbars=yes,screenX=50,screenY=50,top=50');
+	//window.open("/nx-1/img/doctor/doctor_DrKamanaPurohit.jpeg", "DoctorKamana", 'width=200,height=200,scrollbars=yes,screenX=605,screenY=605,top=400');
+	//window.open("/nx-1/img/restaurant/restaurant_MirchMasalaChineseFastFood.jpeg", "RestaurantMirchMasala", 'width=200,height=200,scrollbars=yes,screenX=400,screenY=400,top=400');
+
+	window.open("/img/doctor/doctor_DrKamanaPurohit.jpeg", "DoctorKamana", 'width=200,height=200,scrollbars=yes,screenX=605,screenY=605,top=400');
+	window.open("/img/restaurant/restaurant_MirchMasalaChineseFastFood.jpeg", "RestaurantMirchMasala", 'width=200,height=200,scrollbars=yes,screenX=400,screenY=400,top=400');
 }
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
+
